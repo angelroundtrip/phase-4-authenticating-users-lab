125664712e3ac5505f1439a11858bf89c8eb04fa
Angel Rosario <arosario.us@gmail.com>
2022-03-22 11:56:38 -0400

    Complete

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+  # Find a user in the database using the user id from the session hash
+  # Return the user as a JSON object
+  def show
+    user = User.find_by(id: session[:user_id])
+    render json: user
+  end
+
+end
